72aed08ebc7468155fe7254587c8ebd26cfb58f3
junhong.lee429@gmail.com <junhong.lee429@gmail.com>
2026-04-11 14:21:47 +0900

    First blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeMermaid from 'rehype-mermaid';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://diver-jay.github.io',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		syntaxHighlight: {
+			type: 'shiki',
+			excludeLangs: ['mermaid'],
+		},
+		rehypePlugins: [rehypeMermaid],
+	},
 });
